2eb06e1226051d536a47fde3eb4d0f6f79362775
Leo Dion <leogdion@brightdigit.com>
2017-09-08 21:11:34 -0400

    working on KeyPaths and Codable

--- Pages/KeyPaths.xcplaygroundpage/Contents.swift
+++ Pages/KeyPaths.xcplaygroundpage/Contents.swift
@@ -2,6 +2,13 @@
 
 import Foundation
 
-var str = "Hello, playground"
 
+public struct Meetup : Codable {
+  public let time:Date
+  public let name:String
+  public let description:String
+  public let link:URL
+}
+
+//let meetup = Meetup(time: Date, name: <#T##String#>, description: <#T##String#>, link: <#T##URL#>)
 //: [Next](@next)
--- Pages/Sequences.xcplaygroundpage/Contents.swift
+++ Pages/Sequences.xcplaygroundpage/Contents.swift
@@ -1,5 +1,38 @@
-/*:
-# Advanced Swift 4 Playground
-*/
 
-let str = "asdfasdf"
+/*:
+ # The Advanced Mechanics of Sequence and Collection Types
+ 
+ Swift has a hierarchy of protocols, classes, and structs for
+ handling complex data structures. Here is a brief survey...
+ */
+
+import Darwin
+
+public struct RandomIterator : IteratorProtocol {
+  public typealias Element = UInt32
+  
+  let sequence : RandomSequence
+  var times = 0
+  
+  public init (_ sequence: RandomSequence) {
+    self.sequence = sequence
+  }
+  
+  public mutating func next() -> ItemIterator.Element? {
+    guard times < self.sequence.count else {
+      return nil
+    }
+    times += 1
+    return arc4random()
+  }
+}
+
+public struct RandomSequence : Sequence {
+  public typealias Iterator = RandomIterator
+  
+  let count : Int
+  
+  public func makeIterator() -> RandomIterator {
+    return RandomIterator(self)
+  }
+}
