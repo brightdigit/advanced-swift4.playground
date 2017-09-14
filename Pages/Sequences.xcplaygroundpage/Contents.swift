e75606eaf0fade150c27d361f8943ef75b6c24b0
Leo Dion <leogdion@brightdigit.com>
2017-09-14 15:20:55 -0400

    removed Generics and Sequences Pages

--- Pages/Sequences.xcplaygroundpage/Contents.swift
+++
@@ -1,8 +0,0 @@
-
-/*:
- # The Advanced Mechanics of Sequence and Collection Types
- 
- Swift has a hierarchy of protocols, classes, and structs for
- handling complex data structures. Here is a brief survey...
- */
-
