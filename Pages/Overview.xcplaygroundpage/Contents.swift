6afd4b0d92b6be6456880376d9315af30461c119
Leo Dion <leogdion@brightdigit.com>
2017-09-14 17:55:07 -0400

    updated String Code

--- Pages/Overview.xcplaygroundpage/Contents.swift
+++ Pages/Overview.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@
  This is a playground dedicated to some of the tips and tricks to resolving some of the biggest challenges with Swift 4.
  
  These include:
- * [Strings and Swift 4](Strings)
+ * [Strings are a Pain](Strings)
  * [JSON Encoding in Swift 4](Codable)
  * [Concurrency and the Dispatch Queue](Concurrency)
  */
--- Pages/Strings.xcplaygroundpage/Contents.swift
+++ Pages/Strings.xcplaygroundpage/Contents.swift
@@ -1,45 +1,74 @@
 //: [Previous](@previous)
+/*:
+ ## Strings are a Pain
+ 
+ Some very basic tasks can be fairly difficult. One of those is formatting strings...
+ 
+ ### String Formatting
+ */
 import Foundation
-//An Example of how to treat a String as an array of characters
-
+let location = "🌳🌙"
+//: The easiest way is by using the default method in Swift with the \\(*) notation.
 func meetupWelcomeMessage(forCityOf cityName: String, locatedAt locationName: String) -> String {
   return "🎉Welcome to \(cityName) Cocoaheads, at \(locationName)!!🍾"
 }
+var str = meetupWelcomeMessage(forCityOf: "Ann Arbor", locatedAt: location)
+//: The other way is by using the String.init(format:, arguments) method
+String(format: "🎉Welcome to %@ Cocoaheads, at %@!!🍾", "Ann Arbor",location)
+//: This way is familiar for those who are use to Objective-C, C, and other C-based languages.
+/*:
+ #### Conclusion
+ 
+ Here the reason why you may choose one over the other:
+ 
+ **Native Swift Method**
+ * Cleanest Method and Most Supported
+ * Allows for Further Customization in a Method as Opposed to a Special Syntax.
+ 
+ **C-Style Method**
+ * Best For Those Who Are Already Familiar with C-Style Formatting
+ * Good For Custom Formatting of Numeric Types (Int, Double, etc...)
 
-var str = meetupWelcomeMessage(forCityOf: "Ann Arbor", locatedAt: "🌳🌙")
+ ### Strings are Collections (Swift 4)
+ 
+ 
+ Now you can treat Strings as Character Collections. This means you can access the String with Sequence Closure Methods as well as with String.Index and Range<String.Index>.
+ 
+ #### String.Index and Range Subscript
+ 
+ Which means you use String.Index for:
+ * Accessing Single Characters with Subscripts
+ * Accessing Substrings with Range Subscripts
+ 
+ Here is an example:
 
-String(format: "🎉Welcome to %@ Cocoaheads, at %@!!🍾", "Ann Arbor", "🌳🌙")
-
-// loop through the characters
+ Let's create an index for the last character
+ */
+let indexBeforePopper = str.index(before: str.endIndex)
+//: Then we create an index for the end of the location
+let indexAppleStoreEnd = str.index(indexBeforePopper, offsetBy: -3)
+//: Next we create an index for the start of the location
+let indexAppleStoreStart = str.index(indexAppleStoreEnd, offsetBy: -3-location.count)
+//: Now we can get just the popper character
+let popper = str[indexBeforePopper]
+//: Or we can get the substring of the location
+let appleStore = str[indexAppleStoreStart...indexAppleStoreEnd]
+/*:
+ #### String Closure Methods
+ 
+ We now have access to the set of Collection Methods which loop through each character.
+ */
+//: Of course we can loop through the characters
 for character in str {
   //print(character)
 }
-
-// create an index for the last character
-let indexBeforePopper = str.index(before: str.endIndex)
-
-// create an index for the end of the Apple Store
-let indexAppleStoreEnd = str.index(indexBeforePopper, offsetBy: -3)
-
-// create an index for the start of the Apple Store
-let indexAppleStoreStart = str.index(indexAppleStoreEnd, offsetBy: -6)
-
-// get the popper character
-let popper = str[indexBeforePopper]
-
-// get the substring of the Apple Store
-let appleStore = str[indexAppleStoreStart...indexAppleStoreEnd]
-
-// remove the last two character like an array
+//: But we can do things like use *dropLast* to remove the last two character like an array
 let strWithLessFanFare = str.dropLast(2)
-
-// remove all non-ascii characters
+//: We can use *filter* remove all non-ascii characters
 let strWithLessEmoji = str.filter{ $0.unicodeScalars.contains(where: { $0.isASCII })}
-
-// reverse the string
+//: We can use *reverse*
 let reverseIt = String(str.reversed())
-
-// split the string then mix the characters
+//: We can do other things like split the string then mix the characters
 let splitStrings = str.components(separatedBy: ",")
 zip(splitStrings.first!, splitStrings.last!).reduce("") { (current : String, characters : (Character, Character)) -> String in
   return current.appending(String(current.count % 2 == 1 ? characters.1 : characters.0))
